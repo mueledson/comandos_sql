23fea105f776eb6947c480e873f0bce291ac698e
Edson Emanoel <edson@app100.com.br>
2022-03-05 17:25:53 -0300

    terceiro ou segundo commit

--- jogador.sql
+++ jogador.sql
@@ -1,11 +1,11 @@
 create table tb_jogador(
-    id number primary key auto increment,
-    nome varchar,
-    carteira decimal
+    id int primary key auto_increment,
+    nome varchar(50),
+    carteira decimal(10,2)
 )
+INSERT INTO tb_jogador(nome, carteira) VALUES('muel luis', 3000.50);
+INSERT INTO tb_jogador(nome, carteira) VALUES('sebastiao', 300.50);
+INSERT INTO tb_jogador(nome, carteira) VALUES('tina', 3.50);
 
-
-
-  INSERT INTO tb_jogador(nome, email, senha) VALUES('bastiao', 'bast@gmail.com', '123');
-  UPDATE users SET nome = 'ana maria', email = 'anam@oi.com' WHERE id = 5;
-  DELETE FROM users WHERE id = 1;
+UPDATE tb_jogador SET  carteira = '50.50' WHERE id = 3;
+DELETE FROM tb_jogador WHERE id = 3;
